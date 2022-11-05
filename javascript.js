0f3026cf0e0d2f00051d7dc2cbd6b87d45709e27
Leon Erdesi <109619402+Leon-ER@users.noreply.github.com>
2022-11-05 08:59:31 -0700

    Fixed

--- javascript.js
+++ javascript.js
@@ -1,11 +1,5 @@
 let Password = document.getElementById("password");
 let confirmPassword = document.getElementById("confirmPassword");
-let fName = document.getElementById("firstName");
-let email = document.getElementById("email");
-let passwordOut = document.getElementById("password");
-let lastName = document.getElementById("lastName");
-let phone = document.getElementById("phone");
-let confPass = document.getElementById("confirmPassword");
 
 function pass() {
   if (Password.value !== confirmPassword.value) {
